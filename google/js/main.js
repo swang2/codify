6b91dfb9676d0d50ec03fcec9ea6662e502cec3e
swang2 <swang2@scu.edu>
2016-05-19 02:09:33 -0700

    .

---
+++ google/js/main.js
@@ -0,0 +1,7 @@
+function fun(p1,p2){
+	return p1*p2;
+}
+
+var num1 = prompt("enter first number");
+var num2 = prompt("enter second number");
+console.log(fun(num1,num2));
---
+++ zodiac/js/slider.js
@@ -0,0 +1,42 @@
+// var modal = document.getElementById('poppis');
+// var boton = document.getElementById('boton');
+
+// boton.onclick=function() {
+// 	modal.style.display="block";
+// }
+
+var sign = document.getElementById('input1').value;
+
+if (sign == "pisces") {	
+		$('#boton').click(function() {
+			$('.overlay').css('visibility','visible').click(function() {
+				$('.overlay').css('visibility','hidden');
+			});
+		});
+
+	console.log(sign);
+}
+
+
+// $(document).ready(function(){
+
+// $("#homelink").click(function(){
+//    calcRotation(0);
+// });
+
+// $("#aboutLink").click(function(){
+//   calcRotation(90);
+// });
+
+// $("#newsLink").click(function(){
+//    calcRotation(180);
+// });
+
+// $("#contactLink").click(function(){
+// calcRotation(270);
+// });
+
+// function calcRotation(rot){
+//    $("#cube").css("transform", "rotateY(-" + rot + "deg)");
+// }
+// });
